ac4cab00a86f71917e509ef392a8b467c36b028b
Nkhata-4 <YanjaniNkhata08@outlook.com>
2026-06-27 16:07:33 +0100

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            j = i + 1
+            for j in range(len(nums)):
+                if nums[i] + nums[j] == target and j != i:
+                    ans = [j,i]
+        return ans
+            
